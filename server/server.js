f6d9a1b29e43723db4d1a3cf6fd60d99498dfb05
Sonhoccode <magonsilver05@gmail.com>
2026-02-16 00:08:26 +0700

    Fix proxy error: default port to 8080

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Initialize Socket.io
 const io = initSocket(server);
